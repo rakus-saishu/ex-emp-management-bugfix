a8e5c9f3cca0da0dacd8ab5268a7a50ffae697df
rakus-saishu <157101036+rakus-saishu@users.noreply.github.com>
2024-02-22 11:34:12 +0900

    (7-1)中級 ServiceクラスのJUnitテスト

---
+++ src/test/java/com/example/service/AdministratorServiceTest.java
@@ -0,0 +1,50 @@
+package com.example.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.domain.Administrator;
+import com.example.repository.AdministratorRepository;
+
+@SpringBootTest
+@Transactional
+public class AdministratorServiceTest {
+
+    private AdministratorRepository repository;
+    private AdministratorService service;
+
+    @Autowired
+    public AdministratorServiceTest(AdministratorRepository repository) {
+        this.repository = repository;
+        this.service = new AdministratorService(repository);
+    }
+
+    //管理者登録テスト
+   @Test
+    public void testInsert() {
+        Administrator admin = new Administrator();
+        admin.setName("テスト");
+        admin.setMailAddress("abc@mail");
+        admin.setPassword("testtest");
+        service.insert(admin);
+
+        Administrator insertAdmin = repository.findByMailAddress("abc@mail");
+        assertEquals("テスト", insertAdmin.getName(), "管理者登録テスト失敗");
+        
+    }
+
+    //ログインテスト
+    @Test
+    public void testLogin() {
+        String mail = "iga@sample.com";
+        String pass = "testtest";
+
+        Administrator admin = service.login(mail, pass);
+        assertNotNull(admin);
+        assertEquals("伊賀将之", admin.getName(), "ログインテスト失敗");
+    }
+}
